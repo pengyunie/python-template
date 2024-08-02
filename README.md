4200dc73efef6bb3451d73eac65c01659222c737
Pengyu Nie <prodigy.sov@gmail.com>
2024-08-02 14:49:29 -0400

    improving the template; include seutil by default and some examples of command line args processing

--- README.md
+++ README.md
@@ -1,11 +1,35 @@
-# TODO_project_name
+# Template for Python-based AI+SE research project - TODO: replace it with your project name
 
-To use this template:
-- replace all `TODO_...` with appropriate values
-  - `TODO_pkg_name` is the name of the Python package, should only contain lower case letters, numbers, and `_` (and preferably no `_` at all); don't forget to rename the `src/TODO_pkg_name` directory
-- check all comments start with `TODO:` and take actions
-- test your install:
-  - `./prepare-env.sh`
-  - `conda activate TODO_pkg_name`
-  - `pytest`
-- delete `example_*.py` files and this README's content, and start developing your code
+Steps for using this template:
+
+1. Replace `TODO_pkg_name` across the projects to your package name (based on the project name)
+  * Only lower case letters, numbers, and `_` (not recommended) are allowed in the package name; do NOT use other symbols
+  * Prefer concise names and acronyms, e.g., `teco` instead of `test_completion`
+  * Don't forget to rename the `src/TODO_pkg_name` directory
+
+2. Check the build file `pyproject.toml` and replace as many `TODO_...` as possible
+
+3. Test out your environment
+  * Install [conda](https://docs.anaconda.com/miniconda/) if you haven't
+  * Run `./prepare-env.sh` to create the conda environment
+  * Run `conda activate TODO_pkg_name` to activate the environment
+  * Run `python -m TODO_pkg_name.example_main --help` and see if the code runs (it should work from anywhere)
+  * Run `pytest` to see the tests running (it must be run from the project root `/` or under `/tests`)
+  * Run `conda deactivate` to exit the environment
+
+4. Notes about bundled dependencies:
+
+* `seutil` is a util library. There are some examples to use it in `example_main.py`.
+  * In particular, `jsonargparse`, which is transitively installed by `seutil`, helps to parse command line arguments. Use it to read paths to inputs/outputs from command line, and avoid hardcoding them in code.
+* `black` is a code formatter. Always format your Python code before committing.
+  * Command line: `black .` (run from `/`)
+  * VS Code: install `ms-python.black-formatter` plugin; then use `Format Document` command (`Ctrl+Shift+I` hotkey for me) & `Organize Imports` command (`Ctrl+Shift+O` hotkey for me) to format the code
+* `ruff` is a code linter. Run it periodically to improve your code quality.
+  * Command line: `ruff check .` (run from `/`)
+  * VS Code: install `charliermarsh.ruff` plugin; suggestions should show up in the editor
+
+5. Start developing your code
+  * Replace the example code and this readme with your code/tests/documentation
+  * If you have installed new dependencies using `pip install`, don't forget to add them to `pyproject.toml`
+  * Write [type hints](https://docs.python.org/3/library/typing.html)
+  * Write comments and docstrings
--- src/TODO_pkg_name/example_main.py
+++ src/TODO_pkg_name/example_main.py
@@ -1,2 +1,68 @@
+from pathlib import Path
+from typing import List
+
+import seutil as su
+from jsonargparse import CLI
+
+
 def foo():
+    """A simple (util) function that will be tested in the test file."""
     return 1 + 1
+
+
+class EntryPoint:
+    """
+    This class serves as the entry point from the command line (by using jsonargparse).
+    All the arguments of all functions in this class require type hints! (except for `self`).
+
+    General command line format:
+    ```
+    python -m [fully qualified file name] [class-level args] [function] [function-level args]
+    ```
+
+    Examples for this file:
+    ```
+    python -m TODO_pkg_name.example_main --work_dir ./_work print --inputs=[1,2,3]
+    python -m TODO_pkg_name.example_main print  # use default values
+    ```
+
+    You can also do `python -m TODO_pkg_name.example_main --help` to see the help message.
+    """
+
+    # All arguments specified in the __init__ function are class-level arguments
+    def __init__(
+        self,
+        work_dir: su.arg.RPath = Path(__file__).parent.parent.parent / "_work",
+        # su.arg.RPath: use this type hint for any path (relative or absolute) passed from command line.
+        #   Do NOT use `Path` as the type hint.
+        # You can specify default values of arguments in the function signature using `= value`;
+        #   The default value here points to `/_work` directory under the root of the project.
+    ):
+        self.work_dir = work_dir
+
+    def print(self, inputs: List[int] = None):
+        # If the default value is a mutable data structure (like List), don't write in the function signature;
+        #   write None in the functional signature and check for it at the beginning of the function instead.
+        # Similar to List, there is also Dict, Tuple, Set, etc. that you can import from `typing`.
+        if inputs is None:
+            inputs = [0]
+
+        print(f"work_dir: {self.work_dir}")
+        print(f"inputs: {inputs}")
+        print(f"The result of foo() is {foo()}")
+
+    def read_args(self, f: float, i: int, s: str, strs: List[str]):
+        # This lists some other typical types of arguments you can read from the command line.
+        print(f"f: {f}")
+        print(f"i: {i}")
+        print(f"s: {s}")
+        print(f"strs: {strs}")
+
+
+if __name__ == "__main__":
+    # Using jsonargparse.
+    # The first argument is the entry point class; you can also pass a function instead.
+    # as_positional=False forces the key-value format command line (`--key value` or `--key=value`), which
+    #   I think is more clear; you can set as_positional=True (the default value) for simple command line arguments,
+    #   which allows specifying only values (`value1 value2`) that will be processed according to their positions.
+    CLI(EntryPoint, as_positional=False)
